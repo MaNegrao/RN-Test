0bf53a7ac8674b0a8566e83c258b2bc80b40ec13
Matheus Negrao <matheus.dnegrao@gmail.com>
2019-08-24 15:09:31 -0300

    Fixing error with include of view

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
 import Counter from '../components/Counter';
 
